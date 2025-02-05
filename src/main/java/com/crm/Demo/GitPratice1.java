d81867fe7ba3cae0ce22fbe4e1f04a30c6481ef5
manju <manju@Manjunath_2023>
2025-02-05 12:12:04 +0530

    Hi

--- src/main/java/com/crm/Demo/GitPratice1.java
+++ src/main/java/com/crm/Demo/GitPratice1.java
@@ -4,6 +4,13 @@ public class GitPratice1 {
 	public void Practice2(){
 
 		System.out.println("Practicing pull operation");
+		
+
+	}
+	public void Practice3(){
+
+		System.out.println("Practicing pull operation2");
+		
 
 	}
 
